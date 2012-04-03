fa0d8783e5ada4c88de3aaa780b17f3b983caa36
akiva <akiva@brdesktop>
2012-04-04 01:32:21 +0300

    initial

---
+++ AudioTest.java
@@ -0,0 +1,40 @@
+import sun.audio.*;
+import java.io.*;
+import java.util.Random;
+
+class AudioTest
+{
+	public int getRand(int max)
+	{
+		Random randomGenerator = new Random();
+		return randomGenerator.nextInt(max)+1;
+	}
+	public synchronized void play(String filename) 
+    	{
+        	try 
+		{
+                	InputStream in = new FileInputStream(filename);
+	                AudioStream as = new AudioStream(in);
+	                AudioPlayer.player.start(as); 
+			AudioPlayer.player.join(4000);
+			AudioPlayer.player.stop();
+	        } 
+		catch (Exception e) 
+		{
+	                e.printStackTrace();
+	        }
+    	}
+	public synchronized String getTrack(int num)
+	{
+		return "sounds/"+Integer.toString(num)+".wav";
+	}
+	
+	public static void main(String[] args) 
+	{
+		AudioTest audiotest = new AudioTest();
+		int answer =(audiotest.getRand(3));
+		System.out.println("my answer is: "+answer);
+		audiotest.play(audiotest.getTrack(0));
+		audiotest.play(audiotest.getTrack(answer));
+	}
+}
